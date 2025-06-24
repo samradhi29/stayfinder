f0167767ec45b9a200fcba3c7b1dd3707361dd2b
samradhiR <samradhi4320@gmail.com>
2025-06-24 22:14:47 +0530

    c

--- frontend/src/component/ListingDetails.tsx
+++ frontend/src/component/ListingDetails.tsx
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-const containerStyle = {
-  width: "100%",
-  height: "300px",
-};
+
 
 const ListingDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
